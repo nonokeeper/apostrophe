d804e755c33eefb390e8dfa91f3b8ef0c56e37f5
costearnaud <costearnaud@yahoo.fr>
2023-02-16 09:25:22 +0100

    apos v1

---
+++ modules/@apostrophecms/express/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  options: {
+    session: {
+      // If this still says `undefined`, set a real secret!
+      secret: 'eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY3NjQ4NDcxMywiaWF0IjoxNjc2NDg0NzEzfQ'
+    }
+  }
+};
